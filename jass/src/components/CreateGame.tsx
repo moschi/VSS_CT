5f480dc3210ec8ff9fde95021fe6339093b1429d
David Kühnhanss <david.kuehnhanss@gmail.com>
2020-04-07 10:12:59 +0200

    fixed typo

--- jass/src/components/CreateGame.tsx
+++ jass/src/components/CreateGame.tsx
@@ -1,7 +1,7 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
