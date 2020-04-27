795c169d4e1968780388214c1c2872311eb8baac
unknown <moritzschiesser@gmail.com>
2020-04-27 12:06:39 +0200

    more examples and postman

--- docker/jasserver/app/models/api_game.go
+++ docker/jasserver/app/models/api_game.go
@@ -1,18 +1,28 @@
 package jassmodels
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 // CreateGame ...
 func CreateGame(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	var game Game
+
+	json.NewDecoder(r.Body).Decode(&game)
+
+	tx := database.MustBegin()
+	tx.MustExec("INSERT INTO game (team1, team2, isfinished, createdby) VALUES($1, $2, false, $3)", &game.Teams[0].ID, &game.Teams[1].ID, 0)
+	tx.Commit()
+
 	w.WriteHeader(http.StatusOK)
 }
 
 // DeleteGame ...
 func DeleteGame(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
 	w.WriteHeader(http.StatusOK)
 }
 
--- docker/jasserver/app/models/model_points_per_team_per_round.go
+++ docker/jasserver/app/models/model_points_per_team_per_round.go
@@ -1,9 +1,29 @@
 package jassmodels
 
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
 // PointsPerTeamPerRound ...
 type PointsPerTeamPerRound struct {
 	WiisPoints int64  `json:"wiisPoints,omitempty"`
 	Points     int64  `json:"points,omitempty"`
 	TeamID     int64  `json:"teamId,omitempty"`
 	Round      *Round `json:"round,omitempty"`
+}
+
+func loadPointsPerTeamPerRound(teamID int, roundID int, db *sqlx.DB) PointsPerTeamPerRound {
+	sqlStatement := "SELECT * FROM pointsPerTeamPerRound WHERE teamID = $1 AND roundID = $2"
+	var pointsPerTeamPerRound PointsPerTeamPerRound
+
+	err := db.Get(&pointsPerTeamPerRound, sqlStatement, teamID, roundID)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	*pointsPerTeamPerRound.Round = loadRound(roundID, db)
+
+	return pointsPerTeamPerRound
 }
