33ada7c6c3ce3983503b9dc170ecaa5ee542c73b
David Kühnhanss <david.kuehnhanss@gmail.com>
2020-04-27 17:19:42 +0200

    added static nginx file server

---
+++ docker/jass/build/precache-manifest.3396d0db9a04af84572791ff9f1af220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1428c70b5522a1c381c8617cc84d135e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c49f157b50ca310593ff",
+    "url": "/static/css/main.d638e161.chunk.css"
+  },
+  {
+    "revision": "008a7956151df2c4c996",
+    "url": "/static/js/2.941e9bef.chunk.js"
+  },
+  {
+    "revision": "432ed034d9831dc13cf75201c058fd1d",
+    "url": "/static/js/2.941e9bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49f157b50ca310593ff",
+    "url": "/static/js/main.eb21ec7c.chunk.js"
+  },
+  {
+    "revision": "069790ba5fd748ddfa38",
+    "url": "/static/js/runtime-main.0473e8a2.js"
+  },
+  {
+    "revision": "0c30ab70d02e543d8a27aa1db766cdf2",
+    "url": "/static/media/jasstafel.0c30ab70.jpg"
+  }
+]);
