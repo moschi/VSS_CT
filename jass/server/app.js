c5a728a915571f4322d969d2fce62f37fdcdfc54
David Kühnhanss <david.kuehnhanss@gmail.com>
2020-04-28 10:01:51 +0200

    moved jass folder

--- jass/server/app.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let path = require("path");
-let app = express();
-
-const PATH_TO_STATIC_BUILD_FOLDER = "../build";
-
-app.use(express.static(path.join(__dirname, PATH_TO_STATIC_BUILD_FOLDER)));
-app.listen(8001);
